96eca20eb8fcf5157586d2a4f03bb128e11daac3
Michael Gutman <michaelgutman@hotmail.com>
2022-06-30 14:00:47 -0400

    show the reminder message only if streak is one less than a multiple of configured multiple

--- src/main/java/com/konarreminder/KonarReminderPlugin.java
+++ src/main/java/com/konarreminder/KonarReminderPlugin.java
@@ -3,6 +3,8 @@ package com.konarreminder;
 import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.extern.slf4j.Slf4j;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.ChatMessage;
@@ -39,10 +41,18 @@ public class KonarReminderPlugin extends Plugin
 	@Subscribe
 	public void onChatMessage(ChatMessage chatMessage)
 	{
-		if (chatMessage.getType() == ChatMessageType.GAMEMESSAGE && chatMessage.getMessage().matches(".*You've completed .*\\d tasks.*in a row.*"))
+		String message = chatMessage.getMessage();
+		if (chatMessage.getType() == ChatMessageType.GAMEMESSAGE)
 		{
-			String message = ColorUtil.wrapWithColorTag("You should visit Konar to get bonus points for your next task.", config.chatMessageColor());
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", message, null);
+			Pattern streakPattern = Pattern.compile("\\d+ tasks");
+			Matcher messageMatcher = streakPattern.matcher(message);
+			if (messageMatcher.find()) {
+				int streak = Integer.parseInt(messageMatcher.group().replaceAll("\\D", ""));
+				if ((streak + 1) % config.multiple() == 0) {
+					String reminderMessage = ColorUtil.wrapWithColorTag("You should visit Konar to get bonus points for your next task.", config.chatMessageColor());
+					client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", reminderMessage, null);
+				}
+			}
 		}
 	}
 
