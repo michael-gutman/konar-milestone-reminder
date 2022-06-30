2e8ac463f21b4d711440bef71f7c50e3fa75ae3e
Michael Gutman <michaelgutman@hotmail.com>
2022-06-30 00:18:23 -0400

    project setup

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# Example
-An example greeter plugin
+# Konar Milestone Reminder
+Reminds you to use Konar for more points at milestone slayer tasks
+
+Reminds you before every 10th, 50th, 100th, 250th, or 1000th task to go to konar for your next one
+
+Optionally highlight/mark other slayer masters as an additional reminder.
--- src/main/java/com/konarreminder/KonarReminderPlugin.java
+++ src/main/java/com/konarreminder/KonarReminderPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.konarreminder;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Konar Milestone Reminder"
 )
-public class ExamplePlugin extends Plugin
+public class KonarReminderPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private KonarReminderConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	KonarReminderConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(KonarReminderConfig.class);
 	}
 }
